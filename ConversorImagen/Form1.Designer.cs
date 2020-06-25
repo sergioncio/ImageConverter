0060a253c47e71bdf71d97b9c159b84255803fd7
sergioncio <67414740+sergioncio@users.noreply.github.com>
2020-06-25 12:06:13 +0200

    Add files via upload

---
+++ ConversorImagen/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace ConversorImagen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.Abrir = new System.Windows.Forms.Button();
+            this.Guardar = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.comboBox1.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "*.bmp",
+            "*.jpg",
+            "*.gif",
+            "*.tiff"});
+            this.comboBox1.Location = new System.Drawing.Point(81, 15);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(160, 24);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "*.bmp",
+            "*.jpg",
+            "*.gif",
+            "*.tiff"});
+            this.comboBox2.Location = new System.Drawing.Point(81, 63);
+            this.comboBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(160, 24);
+            this.comboBox2.TabIndex = 1;
+            // 
+            // Abrir
+            // 
+            this.Abrir.Location = new System.Drawing.Point(279, 15);
+            this.Abrir.Margin = new System.Windows.Forms.Padding(4);
+            this.Abrir.Name = "Abrir";
+            this.Abrir.Size = new System.Drawing.Size(117, 28);
+            this.Abrir.TabIndex = 2;
+            this.Abrir.Text = "Abrir";
+            this.Abrir.UseVisualStyleBackColor = true;
+            this.Abrir.Click += new System.EventHandler(this.Abrir_Click);
+            // 
+            // Guardar
+            // 
+            this.Guardar.Location = new System.Drawing.Point(279, 63);
+            this.Guardar.Margin = new System.Windows.Forms.Padding(4);
+            this.Guardar.Name = "Guardar";
+            this.Guardar.Size = new System.Drawing.Size(117, 28);
+            this.Guardar.TabIndex = 3;
+            this.Guardar.Text = "Guardar como";
+            this.Guardar.UseVisualStyleBackColor = true;
+            this.Guardar.Click += new System.EventHandler(this.Guardar_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Location = new System.Drawing.Point(12, 94);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(577, 383);
+            this.panel1.TabIndex = 4;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(0, 3);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(601, 489);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.Guardar);
+            this.Controls.Add(this.Abrir);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.ForeColor = System.Drawing.Color.DarkBlue;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button Abrir;
+        private System.Windows.Forms.Button Guardar;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ ConversorImagen/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace ConversorImagen
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Bitmap img;
+        private void Abrir_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = this.comboBox1.Text + "|" + this.comboBox1.Text;
+            if (!this.comboBox1.Text.Equals(""))
+            {
+                DialogResult dr = ofd.ShowDialog();
+                if (dr == DialogResult.OK)
+                {
+                    MessageBox.Show("Formato válido");
+                    img = new Bitmap(ofd.FileName);
+                    pictureBox2.Location = new System.Drawing.Point(0, 0);
+                    pictureBox2.Name = "pictureBox1";
+                    pictureBox2.Size = panel1.Size;
+                    pictureBox2.SizeMode = PictureBoxSizeMode.AutoSize;
+                    panel1.Controls.Add(pictureBox1);
+                    pictureBox2.Image = img;
+                }
+                else
+                {
+                    MessageBox.Show("El usuario ha cancelado o el formato no es el indicado.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("No ha introducido formato.");
+            }
+        }
+
+        private void Guardar_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = this.comboBox2.Text + "|" + this.comboBox2.Text;
+            if (!this.comboBox2.Text.Equals(""))
+            {
+                if (comboBox2.Text != comboBox1.Text)
+                {
+
+
+                    DialogResult dr = sfd.ShowDialog();
+                    if (dr == DialogResult.OK)
+                    {
+                        if (this.comboBox2.Text.Equals("*.bmp"))
+                        {
+                            img.Save(sfd.FileName, System.Drawing.Imaging.ImageFormat.Bmp);
+                            MessageBox.Show("El archivo se ha guardado correctamente");
+                            pictureBox2.Image = null;
+                            this.comboBox1.Text = null;
+                            this.comboBox2.Text = null;
+                        }
+                        else if (this.comboBox2.Text.Equals("*.jpg"))
+                        {
+                            img.Save(sfd.FileName, System.Drawing.Imaging.ImageFormat.Jpeg);
+                            MessageBox.Show("El archivo se ha guardado correctamente");
+                            pictureBox2.Image = null;
+                            this.comboBox1.Text = null;
+                            this.comboBox2.Text = null;
+                        }
+                        else if (this.comboBox2.Text.Equals("*.gif"))
+                        {
+                            img.Save(sfd.FileName, System.Drawing.Imaging.ImageFormat.Gif);
+                            MessageBox.Show("El archivo se ha guardado correctamente");
+                            pictureBox2.Image = null;
+                            this.comboBox1.Text = null;
+                            this.comboBox2.Text = null;
+                        }
+                        else
+                        {
+                            img.Save(sfd.FileName, System.Drawing.Imaging.ImageFormat.Tiff);
+                            MessageBox.Show("El archivo se ha guardado correctamente");
+                            pictureBox2.Image = null;
+                            this.comboBox1.Text = null;
+                            this.comboBox2.Text = null;
+                        }
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("El usuario ha cancelado la acción.");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("El formato de entrada tiene que ser distinto que el de salida.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("No ha introducido formato.");
+            }
+        }
+        String a = Console.ReadLine();
+    }
+}
